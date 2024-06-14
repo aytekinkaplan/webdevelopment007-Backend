dac2b98f41bac38941f60ee37d1e8fb54c35c1c2
AYTEKİN KAPLAN <112782378+aytekinkaplan@users.noreply.github.com>
2024-06-14 08:49:45 +0300

    commit

--- 2.2 Native Modules/index.js
+++ 2.2 Native Modules/index.js
@@ -1,6 +1,38 @@
 const fs = require("fs");
 
-fs.writeFile("message.txt", "Hello Node", (err) => {
+// message.txt dosyasına yaz
+fs.writeFile("message.txt", "Hello Aytekin Hocam", (err) => {
   if (err) throw err;
   console.log("The file has been saved!");
+});
+
+// message.txt dosyasını oku
+fs.readFile("message.txt", "utf8", (err, data) => {
+  if (err) throw err;
+  console.log(data);
+});
+// index.html dosyasına yaz
+fs.writeFile("main.html", "<h1>Hello My Page</h1>", (err) => {
+  if (err) throw err;
+  console.log("The file has been saved!");
+});
+
+// index.html dosyasını oku
+fs.readFile("main.html", "utf8", (err, data) => {
+  if (err) throw err;
+  console.log(data);
+});
+
+fs.writeFile(
+  "index.html",
+  "<html><head><title>Hello World</title></head><body><h1>Hello World</h1></body></html>",
+  (err) => {
+    if (err) throw err;
+    console.log("The file has been saved!");
+  }
+);
+
+fs.readFile("index.html", "utf8", (err, data) => {
+  if (err) throw err;
+  console.log(data);
 });
